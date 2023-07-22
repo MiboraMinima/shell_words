6da44c2221aa06f3b60663b31c659e40e1108ed7
miboraminima <antoineledoeuff@yahoo.fr>
2023-07-22 04:14:19 +0200

    remove show_def.sh from remote

--- show_def.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-echo -e '
-' >> '/home/miboraminima/Documents/Savoir/projects/shell_words/result/def.txt' 
-head -n 1 '/home/miboraminima/Documents/Savoir/projects/shell_words/result/def.txt' | cowsay -f bud-frogs | lolcat 
-cat '/home/miboraminima/Documents/Savoir/projects/shell_words/result/def.txt'
