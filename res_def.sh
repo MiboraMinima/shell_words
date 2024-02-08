b6509505ddd4fd77579b82e50de6f383ac337a4a
miboraminima <antoineledoeuff@yahoo.fr>
2024-02-09 00:02:32 +0100

    UP: remove test of connection

--- res_def.sh
+++ res_def.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
-    sudo date -s "$(wget --method=HEAD -qSO- --max-redirect=0 google.com 2>&1 | sed -n 's/^ *Date: *//p')" > /dev/null
-else
-    echo "No internet connection."
-fi
-
 dt=$(date)
 
 echo -e "----- $dt\n" >> './result/res_def.txt'
